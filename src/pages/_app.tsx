97f972bccc1d01eedea0dfda436a802417e0f158
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-02-02 22:36:44 +0900

    FIX: next warning _document안에 title 제거
    - https://nextjs.org/docs/messages/no-document-title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "minireset.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>next-youtube</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <title>next-youtube</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
